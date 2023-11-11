4b7f81f22cbb23f0a616b37f32496cabb070b5ca
Kush Rabadia <kushrabadiaz777@gmail.com>
2023-11-10 21:52:01 -0500

    added react redux store to the app

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,9 +1,12 @@
 import CarouselComponent, { Course } from "@/components/carousel";
 import React, { useEffect } from "react";
 import "react-multi-carousel/lib/styles.css";
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
 import Layout from "@/layout/layout";
 
 const Home: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user);
   const [courses, setCourses] = React.useState<Course[]>([]);
 
   useEffect(() => {
