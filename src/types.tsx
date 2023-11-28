2004035a2cba588b2347420eee6839e9b668e395
Kush Rabadia <kushrabadiaz777@gmail.com>
2023-11-28 11:11:37 -0500

    added new tabs component

--- src/types.tsx
+++ src/types.tsx
@@ -6,6 +6,16 @@ export interface User {
   token: string;
 }
 
+export interface Course {
+  _id: string;
+  imageUrl: string;
+  title: string;
+  author: string;
+  playlistId: string;
+  description: string;
+  featured?: boolean;
+}
+
 export interface Storage {
   token: string | null;
   expiryDate: string | null;
